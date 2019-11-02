b4287d25d4c1d27396634b9388378f28bf2f27e5
donqu <itsol>
2019-11-02 22:07:31 +0700

    update

---
+++ src/app/core/models/itemData.ts
@@ -0,0 +1,6 @@
+interface ItemData {
+    id: number;
+    name: string;
+    age: number;
+    address: string;
+  }
--- src/app/entity/rawdata/rawdata.component.ts
+++ src/app/entity/rawdata/rawdata.component.ts
@@ -12,6 +12,14 @@ export class RawdataComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < 100; i++) {
+      this.listOfAllData.push({
+        id: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`
+      });
+    }
   }
 
   optionList = [{ label: 'Lucy', value: 'lucy', age: 20 }, { label: 'Jack', value: 'jack', age: 22 }];
@@ -34,6 +42,51 @@ export class RawdataComponent implements OnInit {
   handleCancel(): void {
       console.log('Button cancel clicked!');
       this.isVisible = false;
+  }
+
+  //table
+  listOfSelection = [
+    {
+      text: 'Select All Row',
+      onSelect: () => {
+        this.checkAll(true);
+      }
+    },
+    {
+      text: 'Select Odd Row',
+      onSelect: () => {
+        this.listOfDisplayData.forEach((data, index) => (this.mapOfCheckedId[data.id] = index % 2 !== 0));
+        this.refreshStatus();
+      }
+    },
+    {
+      text: 'Select Even Row',
+      onSelect: () => {
+        this.listOfDisplayData.forEach((data, index) => (this.mapOfCheckedId[data.id] = index % 2 === 0));
+        this.refreshStatus();
+      }
+    }
+  ];
+  isAllDisplayDataChecked = false;
+  isIndeterminate = false;
+  listOfDisplayData: ItemData[] = [];
+  listOfAllData: ItemData[] = [];
+  mapOfCheckedId: { [key: string]: boolean } = {};
+
+  currentPageDataChange($event: ItemData[]): void {
+    this.listOfDisplayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    this.isAllDisplayDataChecked = this.listOfDisplayData.every(item => this.mapOfCheckedId[item.id]);
+    this.isIndeterminate =
+      this.listOfDisplayData.some(item => this.mapOfCheckedId[item.id]) && !this.isAllDisplayDataChecked;
+  }
+
+  checkAll(value: boolean): void {
+    this.listOfDisplayData.forEach(item => (this.mapOfCheckedId[item.id] = value));
+    this.refreshStatus();
   }
 
 }
